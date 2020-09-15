ab0c48d11a15c222dc9cca31d37f9bee514edfd8
Lucas Norberto Burigo <burigolucas@gmail.com>
2020-09-15 17:29:12 +0200

    Account for multiple image sizes

--- skinLesions/trainClassifierFromTFRecords.py
+++ skinLesions/trainClassifierFromTFRecords.py
@@ -39,12 +39,29 @@ def _augment_image(img, config):
     return img
 
 def _prepare_image(img, config, augment):
+
+    dim = config['img_size']
+
     # Decode jpeg image
     img = tf.image.decode_jpeg(img, channels=3)
     img = tf.cast(img, tf.float32) / 255.0
     
     if augment:
         img = _augment_image(img, config)
+                      
+    img = tf.reshape(img, [dim, dim, 3])
+
+    if config['multiple_size']:
+        dim1 = dim
+        dim2 = int(dim/2)
+        dim3 = int(dim/3)
+        dim4 = int(dim/4)
+        
+        img2 = tf.image.resize(img,[dim2,dim2])
+        img3 = tf.image.resize(img,[dim3,dim3])
+        img4 = tf.image.resize(img,[dim4,dim4])
+        img = (img,img2,img3,img4)
+
     return img
 
 def read_dataset(files,config,augment=False):
@@ -67,17 +84,37 @@ def read_dataset(files,config,augment=False):
     
 def build_model(config):
 
-    dim = config['img_size']
-    inp = keras.layers.Input(shape=(dim,dim,3))
-
-    conv_base = efn.EfficientNetB4(
+    conv_base = efn.EfficientNetB5(
         input_shape=(None,None,3),
         weights='imagenet',
         include_top=False)
     conv_base.trainable = False
 
-    x = conv_base(inp)
-    x = keras.layers.GlobalAveragePooling2D()(x)
+    if config['multiple_size']:
+        dim1 = config['img_size']
+        dim2 = int(dim1/2)
+        dim3 = int(dim1/3)
+        dim4 = int(dim1/4)
+
+        inp1 = keras.layers.Input(shape=(dim1,dim1,3))
+        inp2 = keras.layers.Input(shape=(dim2,dim2,3))
+        inp3 = keras.layers.Input(shape=(dim3,dim3,3))
+        inp4 = keras.layers.Input(shape=(dim4,dim4,3))
+        inp = (inp1,inp2,inp3,inp4)
+
+        x1 = conv_base(inp1, training=False)
+        x1 = keras.layers.GlobalAveragePooling2D()(x1)
+        x2 = conv_base(inp2, training=False)
+        x2 = keras.layers.GlobalAveragePooling2D()(x2)
+        x3 = conv_base(inp3, training=False)
+        x3 = keras.layers.GlobalAveragePooling2D()(x3)
+        x4 = conv_base(inp4, training=False)
+        x4 = keras.layers.GlobalAveragePooling2D()(x4)
+        x = keras.layers.concatenate([x1,x2,x3,x4])
+    else:
+        dim = config['img_size']
+        inp = keras.layers.Input(shape=(dim,dim,3))
+        x = conv_base(inp, training=False)
 
     if config['dropout_rate'] > 0:
         x = keras.layers.Dropout(config['dropout_rate'])(x)
@@ -91,17 +128,7 @@ def build_model(config):
 
     model = keras.Model(inputs=inp,outputs=x)
 
-    return model
-    conv_base.trainable = False
-    x = conv_base(inp)
-    x = keras.layers.GlobalAveragePooling2D()(x)
-    x = keras.layers.Dense(
-        1,
-        activation='sigmoid',
-        bias_initializer = output_bias)(x)
-    model = keras.Model(inputs=inp,outputs=x)
-
-    return model
+    return conv_base, model
 
 def train(model,config,dataloader):
     return model
@@ -123,7 +150,10 @@ def main():
         'nb_epochs': 12,
         'patience': 5,
         'initial_bias': None,
-        'class_weight': None
+        'class_weight': None,
+        'augment': False,
+        'multiple_size': True,
+        'dropout_rate': 0.2,
     }
 
     PATH = f"/data/melanoma/{IMG_SIZE}x{IMG_SIZE}"
@@ -156,7 +186,7 @@ def main():
         'test': ds_test
     }
 
-    model = build_model(config = config)
+    conv_base, model = build_model(config = config)
     model = train(
         model = model,
         dataloader = dataloader,
